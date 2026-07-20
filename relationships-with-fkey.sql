4fbed9602ba739f25d6506f2521df37735b7b650
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-20 16:42:54 +0600

    insert user and post table data

--- relationships-with-fkey.sql
+++ relationships-with-fkey.sql
@@ -12,3 +12,46 @@ CREATE TABLE post (
     title TEXT NOT NULL,
     user_id INTEGER REFERENCES "user" (id)
 );
+
+-- Insert users
+INSERT INTO
+    "user" (username)
+VALUES ('john_doe'),
+    ('emma_wilson'),
+    ('liam_smith'),
+    ('olivia_brown'),
+    ('noah_johnson'),
+    ('ava_davis'),
+    ('william_miller'),
+    ('sophia_moore'),
+    ('james_taylor'),
+    ('isabella_anderson');
+-- Insert posts (random + repeated user_id)
+INSERT INTO
+    post (title, user_id)
+VALUES ('Intro to SQL', 5),
+    ('Mastering PostgreSQL', 2),
+    ('React Basics Guide', 5), -- repeat
+    ('Node.js Backend Setup', 1),
+    (
+        'Database Optimization Tips',
+        10
+    ),
+    (
+        'JavaScript Best Practices',
+        3
+    ),
+    ('Working with REST APIs', 2), -- repeat
+    (
+        'Frontend Development Path',
+        4
+    ),
+    ('Clean Code Principles', 9),
+    (
+        'Fullstack Development Guide',
+        6
+    );
+
+SELECT * FROM "user";
+
+SELECT * FROM post;
