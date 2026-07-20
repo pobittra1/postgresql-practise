9d79ee1c31939f1dbab8be08691b99af299513c1
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-20 18:57:09 +0600

    explore LEFT OUTER and RIGHT OUTER JOIN

--- join.sql
+++ join.sql
@@ -3,6 +3,7 @@ SELECT * FROM "user";
 SELECT * FROM post;
 
 -- retrive all posts title with their username
+-- INNER JOIN
 -- in user table don't have any post title and in post table don't have user name so we need to join it
 SELECT title, username
 FROM post
@@ -11,3 +12,24 @@ FROM post
 SELECT title, username
 FROM post AS p
     INNER JOIN "user" AS u on p.user_id = u.id;
+
+-- INSERT NULL value for post.user_id of post table
+-- at first drop the not null constraint then insert null value.
+ALTER TABLE post ALTER COLUMN user_id DROP NOT NULL;
+
+INSERT INTO
+    post (id, title, user_id)
+VALUES (
+        21,
+        'this is the test post',
+        NULL
+    );
+
+-- LEFT OUTER JOIN
+SELECT title, username
+FROM post AS p
+    LEFT OUTER JOIN "user" AS u on p.user_id = u.id;
+-- RIGHT OUTER JOIN
+SELECT title, username
+FROM post AS p
+    RIGHT OUTER JOIN "user" AS u on p.user_id = u.id;
