2fd4f36b6b2dc29af5870b63145ae9c93e5ce5ce
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-21 10:09:10 +0600

    solve new orders table problems

--- practices/practise.sql
+++ practices/practise.sql
@@ -288,3 +288,73 @@ SELECT EXTRACT(
 FROM employees
 GROUP BY
     hired_year;
+
+-- ---------practise more---------------
+-- create orders table
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+-- Inserting sample data into the orders table
+INSERT INTO
+    orders (
+        customer_id,
+        order_date,
+        total_amount
+    )
+VALUES (1, '2022-01-05', 100.50),
+    (2, '2022-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2022-01-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2022-02-01', 250.50),
+    (1, '2022-02-05', 180.25);
+
+-- see orders
+SELECT * FROM orders;
+
+-- Task 6. Find customers who have placed more than 2 orders and calculate the total amount spent by each of these customers.
+-- -----For this example
+-- 1. FROM: get data from orders table
+-- 2. GROUP BY: group rows by customer_id
+-- 3. HAVING: keep only customers with > 2 orders
+-- 4. SELECT: calculate total_orders & total_spent
+SELECT
+    customer_id,
+    COUNT(*) AS total_orders,
+    sum(total_amount) as total_spent
+FROM orders
+GROUP BY
+    customer_id
+HAVING
+    COUNT(*) > 2;
+;
+
+-- Task 7. Find the total amount of orders placed each month in the year 2022.
+
+-- -------For this Example
+-- 1. FROM: get data from table
+-- 2. WHERE: filter only 2022 data
+-- 3. GROUP BY: group by month
+-- 4. SELECT: calculate month & total amount
+-- 5. ORDER BY: sort result by month
+SELECT
+    EXTRACT(
+        MONTH
+        FROM order_date
+    ) AS order_month_of_2022,
+    SUM(total_amount) AS total_spent
+FROM orders
+WHERE
+    EXTRACT(
+        YEAR
+        FROM order_date
+    ) = 2022
+GROUP BY
+    order_month_of_2022;
